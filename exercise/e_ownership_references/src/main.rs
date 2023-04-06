0d8af34c65a18fbe31ec315b21ec885536d75c08
nikolas lunkes <nikolas.s.lunkes@gmail.com>
2023-04-06 15:13:10 -0300

    more exercices made

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -14,26 +14,37 @@ fn main() {
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
+    let mut s:String = String::from("batatinha");
+    no_borrow_test(s.clone());
+    println!("{}",s);
+    borrow_test(&mut s);
+    println!("{}",s);
+    
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
     //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    if eat(arg.clone()) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
 
+
+    let mut structest:RedFox = RedFox::new();
+    println!("The fox life is: {}",structest.life);
+    structest.eat();
+    println!("The fox life has changed to: {}",structest.life);
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
     // Challenge: Write a function "bedazzle" that takes a mutable reference to a String and
@@ -43,8 +54,57 @@ fn main() {
     // Hint: You will need to dereference the mutable reference in order to assign it a
     // new value.
     //
-    // let mut material = "mud".to_string();
-    // println!("This material is just `{}`.", material);
-    // bedazzle(&mut material);
-    // println!("Wow! Now the material is `{}`!", material);
+    let mut material = "mud".to_string();
+    println!("This material is just `{}`.", material);
+    bedazzle(&mut material);
+    println!("Wow! Now the material is `{}`!", material);
+}
+
+pub fn inspect(s:&str) {
+    if s.ends_with("s"){
+        println!("This string ends with s");
+    }else{
+        println!("This string does not end with s");
+    }
+}
+
+pub fn change(s:&mut String) {
+    if !s.ends_with("s"){
+        s.push_str("s")
+    }
+}
+
+pub fn eat(s:String) -> bool {
+    return s.starts_with("b") && s.contains("a") 
+        
+}
+
+pub fn bedazzle(s:&mut String){
+    *s = String::from("sparkly");
+}
+
+pub fn borrow_test(s: &mut String){
+    *s= s.to_uppercase()+"s";
+    println!("{}",s);
+}
+
+pub fn no_borrow_test(s:String){
+    let mut s:String = s;
+    s = s.to_uppercase();
+    println!("{}",s);
+}
+
+
+struct RedFox {
+    enemy: bool,
+    life: i32,
+}
+
+impl RedFox {
+    fn new() -> Self {
+        Self { enemy: true, life: 70 }
+    }
+    fn eat(&mut self) {
+        self.life += 10;
+    }
 }
