170d78f2fe2eb2be0552bc23b71dc5588f7e646f
NikolasSilvaLunkes <nikolaslunkes@gmail.com>
2023-05-21 14:18:18 -0300

    finished_closures

--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -1,7 +1,7 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_imports, unused_variables)]
 use crossbeam::channel;
-use std::thread;
+use std::{thread, result};
 use std::time::Duration;
 
 fn expensive_sum(v: Vec<i32>) -> i32 {
@@ -28,14 +28,14 @@ fn pause_ms(ms: u64) {
 }
 
 fn main() {
-    let my_vector:Vec<i32> = vec![2, 5, 1, 0, 4, 3];
+    let my_vector:Vec<i32> = vec![2, 5, 1, 0, 4, 3, 7, 2, 8, 9];
 
     // 2. Spawn a child thread and have it call `expensive_sum(my_vector)`.  Store the returned
     // join handle in a variable called `handle`. Once you've done this you should be able to run
     // the code and see the Child thread output in the middle of the main thread's letters
     //
-    //let handle = ...
-    println!("{}",expensive_sum(my_vector));
+    let handle = thread::spawn(move || expensive_sum(my_vector));
+    //println!("{}",expensive_sum(my_vector));
 
     // While the child thread is running, the main thread will also do some work
     for letter in vec!["a", "b", "c", "d", "e", "f"] {
@@ -48,8 +48,8 @@ fn main() {
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
     //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+    let sum = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
@@ -59,7 +59,7 @@ fn main() {
     // flow of execution works.  Once you understand it, alter the values passed to the `pause_ms()`
     // calls so that both the "Thread B" outputs occur before the "Thread A" outputs.
 
-    /*
+    
     let (tx, rx) = channel::unbounded();
     // Cloning a channel makes another variable connected to that end of the channel so that you can
     // send it to another thread.
@@ -92,7 +92,7 @@ fn main() {
     // Join the child threads for good hygiene.
     handle_a.join().unwrap();
     handle_b.join().unwrap();
-    */
+    
 
     // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
     // main thread loop through several values and print each out and then send it to the channel.
